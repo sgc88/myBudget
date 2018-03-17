564be99fa7c9947aca1ed4846be392596ec583f5
sgc88 <sehribancheng@yahoo.com>
2018-03-17 00:21:01 -0700

    our check button works clickable

--- app.js
+++ app.js
@@ -10,17 +10,20 @@ var UIController = (function(){
 
 
 var controller  = (function(budgetCtrl, UICtrl){
-document.querySelector(".add__btn").addEventListener("click", function(){
+  var ctrlAddItem = function(){
+    console.log("working");
+  }
+document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
   // var money = document.getElementById('amount').value;
   //1. get the filed input data
   //2 add the item to budget controller
   //3 add the item to the UI
   //4 calculate the budget
   //5 Display the budget on the UI
-});
+
 document.addEventListener("keypress", function(event){
 if(event.keyCode === 13){
-  console.log("Enter was pressed");
+  ctrlAddItem();
 }
 });
 
