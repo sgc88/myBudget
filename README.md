bafda8ac63b4ce1d231a28098e2377b6c1731560
sgc88 <sehribancheng@yahoo.com>
2018-03-16 23:57:07 -0700

    setting up event listener keycode

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # myBudget
 # this app is about our budget and how to manage our money better
+* using http://keycode.info/ for keycode 
--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("hello world");
+var budgetController = (function(){
+  //some code here
+})();
+
+var UIController = (function(){
+
+  //some code here
+})();
+
+
+var controller  = (function(budgetCtrl, UICtrl){
+document.querySelector(".add__btn").addEventListener("click", function(){
+  // var money = document.getElementById('amount').value;
+  //1. get the filed input data
+  //2 add the item to budget controller
+  //3 add the item to the UI
+  //4 calculate the budget
+  //5 Display the budget on the UI
+});
+document.addEventListener("keypress", function(event){
+if(event.keyCode === 13){
+  console.log("Enter was pressed");
+}
+});
+
+})(budgetController, UIController);
